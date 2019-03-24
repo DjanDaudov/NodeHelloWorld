ee4b0054b74e0b0218917ad12cbdaed74d8f5dfb
Denis Stagniunas <denis.stagniunas@gmail.com>
2019-03-24 13:29:03 +0200

    Hello 13:28

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello 19:511 NodeApp!! \n");
+  response.end("Hello 13:28 NodeApp!! \n");
 });
 
 
