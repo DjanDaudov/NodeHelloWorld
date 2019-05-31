627da09a1d8f680b17b93078d0e138f58c9bcb31
Denis Stagniunas <denis.stagniunas@gmail.com>
2019-05-31 18:15:30 +0300

    Gynimaiu test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Gynimas0529 \n");
+  response.end("Gyniumai test \n");
 });
 
 
